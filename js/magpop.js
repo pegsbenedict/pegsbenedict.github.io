912c17905c1fc78b0a3186fa716f97c6e613b59f
Margo Yoon <margo@margopixelated.com>
2014-12-15 16:58:17 -0600

    finally fixed the goddamn magnific.js lightbox gallery

---
+++ js/magpop.js
@@ -0,0 +1,20 @@
+$('.open-popup').magnificPopup({
+    items: [
+      {
+        src: 'images/TNP/TNP_userpersona-BEN.png',
+        title: 'User Persona Ben'
+      },
+      {
+        src: 'images/TNP/TNP_userpersona-DREW.png',
+        title: 'User Persona Drew'
+      },
+    {
+        src: 'images/TNP/TNP_userpersona-MONA.png',
+        title: 'User Persona Mona'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
